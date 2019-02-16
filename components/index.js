1fa51172a4117405640d4fdaaece08dadcaf291e
Joshua Kaluba <joshkaluba@gmail.com>
2019-02-15 22:30:50 -0500

    Created index listing all exports

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import PrimaryButton from './PrimaryButton';
+import DeleteButton from './DeleteButton';
+import PrimaryFormInput from './PrimaryFormInput';
+import StyledText from './StyledText';
+import TabBarIcon from './TabBarIcon';
+import ArmLocationButton from './ArmLocationButton';
+
+export {
+    PrimaryButton,
+    DeleteButton,
+    PrimaryFormInput,
+    StyledText,
+    TabBarIcon,
+    ArmLocationButton
+}
